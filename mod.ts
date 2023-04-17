9a80484bc60f1592f3fb0222a783095d9d6ba1ae
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-17 16:18:26 +0900

    feat(mod): export module to entrypoint

---
+++ mod.ts
@@ -0,0 +1,12 @@
+// Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export { type Options, permissionsPolicy } from "./middleware.ts";
+export { type Handler, type Middleware } from "./deps.ts";
+export type {
+  ExperimentalFeatures,
+  PolicyControlledFeatures,
+  ProposedFeatures,
+  StandardizedFeatures,
+} from "./features.ts";
+export type { AllowList, PermissionsPolicy } from "./types.ts";
